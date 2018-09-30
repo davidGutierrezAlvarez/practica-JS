19a9f0c233e001f15e6cdf5ae6acf00f6221724b
david <david_xd1996@live.com.mx>
2018-09-30 00:05:26 -0500

    mas cambios

--- ejemplo.js
+++ ejemplo.js
@@ -1,7 +1,7 @@
 var num1,num2,suma;
 
 
-while (isNaN(num1) || num1=="") {
+while (isNaN(num1) || num1==="" || num1===null || typeof num1===undefined) {
     //document.write("eso no es un numero");
     alert("no es un numero");
     num1 = prompt("escribe el primer numero: ");
@@ -17,3 +17,5 @@ document.write(num1+" / "+num2+" = "+num1/num2+"</br>");
 document.write(num1+" + "+num2+" = "+ suma +"</br>");
 document.write(num1+" - "+num2+" = "+(num1-num2)+"</br>");
 //document.write(num2+num1);
+
+var numeros = [1,2,3,4,5,6];
